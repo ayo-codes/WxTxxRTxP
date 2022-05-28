261a417a12e4cb03bdee5076331908bad57fc0db
ayo-codes <onamusia@gmail.com>
2022-05-29 00:42:41 +0100

    UPDATED app to allow for the app to have members and link data to a member

--- app/utils/StationAnalytics.java
+++ app/utils/StationAnalytics.java
@@ -5,12 +5,13 @@ import java.util.List;
 public class StationAnalytics {
 
     public static Reading getLatestReading(List<Reading> readings) {
-       int lastIndex = readings.size() - 1;
-       readings.get(lastIndex);
-       Reading latestReadings = readings.get(lastIndex);
-       System.out.println(latestReadings);
-        return latestReadings;
+        if ((readings != null) && (readings.isEmpty() == false)) {
+            int lastIndex = readings.size() - 1;
+            readings.get(lastIndex);
+            Reading latestReadings = readings.get(lastIndex);
+            System.out.println(latestReadings);
+            return latestReadings;
+        } else
+            return null;
     }
 }
-
-
